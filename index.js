abcb9286d48be7df26e40b2a0c7b9e37abd16e95
Joshua Sandlin <joshua.sandlin@gmail.com>
2016-04-02 21:59:18 -0400

    removed var

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-var videoFile = 'https://s3-us-west-2.amazonaws.com/nbl-egghead/01-egghead-angularjs-angular-with-webpack-introduction.mp4';
-
 var angular = require('angular');
 var app = angular.module('app', []);
 
